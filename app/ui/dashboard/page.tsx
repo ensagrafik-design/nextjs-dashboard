1f4c65e06b3d321788618993bb9b03ce305c3e55
ensagrafik-design <ensagrafik@gmail.com>
2025-09-21 18:44:14 +0300

    first commit

---
+++ app/ui/dashboard/page.tsx
@@ -0,0 +1,26 @@
+import { fetchCardData } from '@/app/lib/data';
+import { fetchRevenue } from '@/app/lib/data';
+
+export default async function Page() {
+  const revenue = await fetchRevenue();
+  const {
+    totalPaidInvoices,
+    totalPendingInvoices,
+    numberOfInvoices,
+    numberOfCustomers,
+  } = await fetchCardData();
+
+  return (
+    <main>
+      <section>
+        <h1>Revenue</h1>
+        <p>{revenue}</p>
+      </section>
+      <section>
+        <h1>Invoice Summary</h1>
+        <p>{numberOfInvoices} invoices</p>
+        <p>{numberOfCustomers} customers</p>
+      </section>
+    </main>
+  );
+}
